a6a2ddfe3bf8b91fbbc89d7c51421c379f6bb0a6
Jeremy Landis <jeremylandis@hotmail.com>
2023-03-10 11:20:33 -0500

    [license] Automate license application

--- src/main/java/com/github/dantwining/whitespace/VerifyMojo.java
+++ src/main/java/com/github/dantwining/whitespace/VerifyMojo.java
@@ -1,3 +1,17 @@
+/*
+ * whitespace-maven-plugin (https://github.com/hazendaz/whitespace-maven-plugin)
+ *
+ * Copyright 2011-2023 dantwining, Hazendaz.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of The Apache Software License,
+ * Version 2.0 which accompanies this distribution, and is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * Contributors:
+ *     dantwining (dantwining).
+ *     Hazendaz (Jeremy Landis).
+ */
 package com.github.dantwining.whitespace;
 
 /*
