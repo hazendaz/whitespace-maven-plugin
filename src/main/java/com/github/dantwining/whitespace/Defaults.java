01422cfeb354d852e989b76e241979eb2478639c
Jeremy Landis <jeremylandis@hotmail.com>
2025-11-16 17:11:16 -0500

    [extensions] Remove 'reg' as most modern registry files will be utf-16le

--- src/main/java/com/github/dantwining/whitespace/Defaults.java
+++ src/main/java/com/github/dantwining/whitespace/Defaults.java
@@ -19,7 +19,7 @@ public final class Defaults {
     /**
      * Default extension list for both trim and verify mojos.
      */
-    public static final String WHITESPACE_DEFAULT_EXTENSIONS = "4gl,42m,c,cb,cfg,cpp,css,dat,groovy,h,html,ini,java,js,json,jsonp,jsp,jspx,jsx,ksh,kt,kts,md,mk,msg,pl,pm,properties,reg,scala,scss,sh,sql,toml,ts,tsx,wadl,wsdl,xhtml,xml,xsd,yaml,yml";
+    public static final String WHITESPACE_DEFAULT_EXTENSIONS = "4gl,42m,c,cb,cfg,cpp,css,dat,groovy,h,html,ini,java,js,json,jsonp,jsp,jspx,jsx,ksh,kt,kts,md,mk,msg,pl,pm,properties,scala,scss,sh,sql,toml,ts,tsx,wadl,wsdl,xhtml,xml,xsd,yaml,yml";
 
     /**
      * Prevent instantiation of this class.
